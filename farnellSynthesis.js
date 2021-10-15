c4a1a2f6db1d8db90897ae33d159699cf1b30635
Sedona Thomas <sntsregister@gmail.com>
2021-10-15 14:21:35 -0400

    Update farnellSynthesis.js

--- farnellSynthesis.js
+++ farnellSynthesis.js
@@ -4,12 +4,15 @@
  */
 
 var audioCtx;
+
 var lowpass1;
 var freq1 = 400;
+
 var lowpass2;
 var freq2 = 14;
 var lowpass2Mult = 400;
 var lowpass2Add = 500;
+
 var highpass;
 var rq = 0.03;
 var mul = 0.1;
@@ -21,8 +24,7 @@ function play(event) {
         audioCtx = initAudio();
         highpass = initHighpass();
         lowpass1 = initLowpass(freq1, highpass);
-        lowpass2 = initLowpass2(freq2);
-        lowpass2.connect(highpass.frequency);
+        lowpass2 = initLowpass2(freq2, highpass);
         return;
     }
     else if (audioCtx.state === 'suspended') {
@@ -57,7 +59,7 @@ function initLowpass(freq, highpass) {
     return lowpass;
 }
 
-function initLowpass2(freq) {
+function initLowpass2(freq, highpass) {
     var brown = makeBrownNoise();
     lowpass = audioCtx.createBiquadFilter();
     lowpass.type = "lowpass";
@@ -74,7 +76,7 @@ function initLowpass2(freq) {
     constantSource.connect(gainNode.gain);
     constantSource.start();
 
-    gainNode.connect(audioCtx.destination);
+    gainNode.connect(highpass);
     return lowpass;
 }
 
@@ -97,76 +99,3 @@ function makeBrownNoise() {
     brownNoise.start(0);
     return brownNoise;
 }
-
-
-
-
-
-/*
-const playButton = document.getElementById("play");
-playButton.addEventListener('click', play, false);
-function play(event) {
-    if (!audioCtx) {
-        audioCtx = initAudio();
-        lowpass1 = initLowpass(freq1);
-        highpass = initHighpass(lowpass1);
-        lowpass2 = initLowpass(freq2);
-        lowpass2.connect(highpass.frequency);
-        return;
-    }
-    else if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
-    }
-    else if (audioCtx.state === 'running') {
-        audioCtx.suspend();
-    }
-}
-
-function initAudio() {
-    return new (window.AudioContext || window.webkitAudioContext)();
-}
-
-function initHighpass(lowpass) {
-    var high = audioCtx.createBiquadFilter();
-    high.type = "highpass";
-    high.frequency.setValueAtTime(0, audioCtx.currentTime);
-    high.gain.setValueAtTime(0, audioCtx.currentTime);
-    highpass.connect(audioCtx.destination);
-    high.Q.value = 1 / rq;
-    return high;
-}
-
-function initLowpass(freq) {
-    var brown = makeBrownNoise();
-    lowpass = audioCtx.createBiquadFilter();
-    lowpass.type = "lowpass";
-    lowpass.frequency.setValueAtTime(freq, audioCtx.currentTime);
-    lowpass.gain.setValueAtTime(0, audioCtx.currentTime);
-    brown.connect(lowpass).connect(audioCtx.destination);
-    return lowpass;
-}
-
-function makeBrownNoise() {
-    var bufferSize = 10 * audioCtx.sampleRate,
-        noiseBuffer = audioCtx.createBuffer(1, bufferSize, audioCtx.sampleRate),
-        output = noiseBuffer.getChannelData(0);
-
-    var lastOut = 0;
-    for (var i = 0; i < bufferSize; i++) {
-        var brown = Math.random() * 2 - 1;
-        output[i] = (lastOut + (0.02 * brown)) / 1.02;
-        lastOut = output[i];
-        output[i] *= 3.5;
-
-        //output[i] = output[i] * 400 + 500; //directly change freq?
-    }
-
-    brownNoise = audioCtx.createBufferSource();
-    brownNoise.buffer = noiseBuffer;
-    brownNoise.loop = true;
-    brownNoise.start(0);
-    return brownNoise;
-}
-
-
-*/
